35f800f57bc7d6526075271735728da76332ab8d
balo <balo@alexandria.pc>
2025-07-23 01:48:48 +0100

    Hot fix

--- app/client/src/providers/WalletProvider.tsx
+++ app/client/src/providers/WalletProvider.tsx
@@ -3,7 +3,6 @@ import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { useMemo } from 'react';
-import { clusterApiUrl } from '@solana/web3.js';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
